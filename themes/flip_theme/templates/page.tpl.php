d7bad41e6d9dfa680b677eae0213ac5023086152
summerscope <summerscope@gmail.com>
2014-03-28 11:58:20 +1100

    Remove unnecessary fonts. Adding theme images and icons. start to css theming for project leap.

--- themes/flip_theme/templates/page.tpl.php
+++ themes/flip_theme/templates/page.tpl.php
@@ -1,4 +1,4 @@
-<h1>(*** FLIP THEME ***)</h1>
+<p>(*** FLIP THEME ***)</p>
 <div id="skipnav" class="element-invisible">
   <div class="container">
     <p>Skip to:</p>
@@ -40,7 +40,7 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 <script type='text/javascript' src='http://os.alfajango.com/js/jquery.easytabs.min.js'></script>
 <script type='text/javascript' src='http://os.alfajango.com/js/raptorize/jquery.raptorize.1.0.js'></script>
 <script type='text/javascript' src='http://os.alfajango.com/js/jquery.easing.1.3.js'></script>
-
+<script type="text/javascript" src="http://fast.fonts.net/jsapi/edd5a0b1-f0fd-4fd8-a316-5aa9bf5765ed.js"></script>
 
 
 
