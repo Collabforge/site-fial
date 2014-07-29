9ff0400f25a9bf5b8e5fc27a2fc69fd51ca5f9e2
Matt Withoos <mattwithoos@gmail.com>
2014-07-29 13:01:10 +1000

    menu part 15 - new fonts

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -40,7 +40,7 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 <script type='text/javascript' src='http://os.alfajango.com/js/jquery.easytabs.min.js'></script>
 <script type='text/javascript' src='http://os.alfajango.com/js/raptorize/jquery.raptorize.1.0.js'></script>
 <script type='text/javascript' src='http://os.alfajango.com/js/jquery.easing.1.3.js'></script>
-
+<script type="text/javascript" src="http://fast.fonts.net/jsapi/b23760be-4be4-4e65-b79d-771dcea8a674.js"></script>
 
 
 
