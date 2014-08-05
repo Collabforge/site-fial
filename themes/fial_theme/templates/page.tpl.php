e705f453e3efb3a4b2e872ec9d9d995c13b3547b
Matt Withoos <mattwithoos@gmail.com>
2014-08-05 16:23:17 +1000

    jquery expander part 3

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -41,7 +41,7 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 <script type='text/javascript' src='http://os.alfajango.com/js/raptorize/jquery.raptorize.1.0.js'></script>
 <script type='text/javascript' src='http://os.alfajango.com/js/jquery.easing.1.3.js'></script>
 <script type="text/javascript" src="http://fast.fonts.net/jsapi/b23760be-4be4-4e65-b79d-771dcea8a674.js"></script>
-<script type="text/javascript" src="http://fial.collabcoapp.com/profiles/collabco/libraries/jquery-expander/jquery-expander.js"></script>
+<script type="text/javascript" src="http://fial.collabcoapp.com/profiles/collabco/libraries/jquery-expander/jquery.expander.js"></script>
 <script type="text/javascript">
 // you can override default options globally, so they apply to every .expander() call
 $.expander.defaults.slicePoint = 120;
