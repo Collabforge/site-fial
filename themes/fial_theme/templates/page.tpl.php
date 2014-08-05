bf7ae91c6f2d4e33a641142ff21ad041e504efad
Matt Withoos <mattwithoos@gmail.com>
2014-08-05 12:42:02 +1000

    new header part 1

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -161,6 +161,12 @@ jQuery(document).ready(function($) {
 <?php endif; ?>
 <!-- End of new Header Top block -->
 
+<div id="footer" class="clearfix site-footer" role="contentinfo">
+      <!--<div class="container" style="margin-top:-40px;">-->
+      <h1 class="title" id="page-title"> <?php print $title; ?> </h1>
+      <!--</div>-->
+</div>
+
 <div id="main" class="clearfix main" role="main">
   <div class="container">
     <?php if ($breadcrumb): ?><?php echo $mobile['block_menu']; ?>
@@ -175,11 +181,7 @@ jQuery(document).ready(function($) {
     <div id="main-upper" class="row-fluid main-upper"> <?php print render($page['main_upper']); ?> </div>
     <?php endif; ?>
 
-    <div id="footer" class="clearfix site-footer" role="contentinfo">
-      <!--<div class="container" style="margin-top:-40px;">-->
-      <h1 class="title" id="page-title"> <?php print $title; ?> </h1>
-      <!--</div>-->
-    </div>
+
 
     <div id="main-content" class="row main-content">
       <?php if ($page['sidebar_first']): ?>
