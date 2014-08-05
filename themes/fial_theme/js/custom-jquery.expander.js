09bd2eed771c1b45ff71bf85bb209c3a88b0df2c
Matt Withoos <mattwithoos@gmail.com>
2014-08-05 16:45:13 +1000

    expander final

--- themes/fial_theme/js/custom-jquery.expander.js
+++ themes/fial_theme/js/custom-jquery.expander.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
   // override default options (also overrides global overrides)
   $('div.expandable').expander({
-    slicePoint:       80,  // default is 100
+    slicePoint:       500,  // default is 100
     expandPrefix:     ' ', // default is '... '
     expandText:       '[...]', // default is 'read more'
     collapseTimer:    5000, // re-collapses after 5 seconds; default is 0, so no re-collapsing
