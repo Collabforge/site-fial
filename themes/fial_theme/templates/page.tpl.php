832b2a295e22afa3f2260bc17a1d707042231bc9
Matt Withoos <mattwithoos@gmail.com>
2014-08-08 16:00:34 +1000

    new stuff v3

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -44,8 +44,6 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 
 <!--Drupal Jquery (1.8)-->
 
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.infinitescroll.min.js"></script>
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.autopager.js"></script>
 
 <!--Custom Jquery (2.1)-->
 
