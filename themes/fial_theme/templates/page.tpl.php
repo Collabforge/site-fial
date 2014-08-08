1ed80db76dff448f7c56b1845a319409eee6ea8d
Matt Withoos <mattwithoos@gmail.com>
2014-08-08 14:41:02 +1000

    infinite scrolling for founding members page part 3

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -41,11 +41,13 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 <script type='text/javascript' src='http://os.alfajango.com/js/raptorize/jquery.raptorize.1.0.js'></script>
 <script type='text/javascript' src='http://os.alfajango.com/js/jquery.easing.1.3.js'></script>
 <script type="text/javascript" src="http://fast.fonts.net/jsapi/b23760be-4be4-4e65-b79d-771dcea8a674.js"></script>
+
+<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.autoloader.js"></script>
+<script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.infinitescroll.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="http://fial.collabcoapp.com/profiles/collabco/libraries/jquery-expander/jquery.expander.js"></script>
 <script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.expander.js"></script>
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.autoloader.js"></script>
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.infinitescroll.min.js"></script>
+
 
 <div class="ui-mobile-menu visible-phone hidden-desktop">
 
