985173b0cd9fd14d3655541671c76992e2a1df0c
Matt Withoos <mattwithoos@gmail.com>
2014-08-08 14:43:22 +1000

    infinite scrolling for founding members page part 3.2

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -42,8 +42,11 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 <script type='text/javascript' src='http://os.alfajango.com/js/jquery.easing.1.3.js'></script>
 <script type="text/javascript" src="http://fast.fonts.net/jsapi/b23760be-4be4-4e65-b79d-771dcea8a674.js"></script>
 
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.autopager.js"></script>
+<!--Drupal Jquery (1.8)-->
 <script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.infinitescroll.min.js"></script>
+<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.autopager.js"></script>
+
+<!--Custom Jquery (2.1)-->
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="http://fial.collabcoapp.com/profiles/collabco/libraries/jquery-expander/jquery.expander.js"></script>
 <script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.expander.js"></script>
