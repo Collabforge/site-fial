e1dc88ca957a8fd6be3a129470cc1a9e5d8f0a73
Matt Withoos <mattwithoos@gmail.com>
2014-08-08 17:12:12 +1000

    new stuff v8.6

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -48,7 +48,8 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 <!--Custom Jquery, needs manual adding due to Drupal hooks-->
 <?php
 // Checks if current page is jquery-incompatible
-if (current_path() != "NODE/14") { ?>
+$current_path = current_path();
+if ($current_path != "NODE/14") { ?>
 <script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
 <script type="text/javascript" src="/sites/default/libraries/jquery-expander/jquery.expander.js"></script>
 <script type="text/javascript" src="/sites/default/libraries/custom-jquery-expander/custom-jquery.expander.js"></script>
