801c77bdbdf8c199d973106b865df9c5a0df6886
Matt Withoos <mattwithoos@gmail.com>
2014-08-12 17:58:30 +1000

    fixing jquery v5

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -48,7 +48,7 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 <!--Custom Jquery, needs manual adding due to Drupal hooks-->
 <?php
 // Checks if current page is jquery-incompatible
-echo current_path();
+//echo current_path();
 if (current_path() == "node/463") { ?>
 <script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
 <script type="text/javascript" src="/sites/default/libraries/jquery-expander/jquery.expander.js"></script>
