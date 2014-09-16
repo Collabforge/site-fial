b26281016f2e51ce8ce37da9189af4ed66ab3a83
Matt Withoos <mathewwithoos@MATHEWs-MacBook-Air.local>
2014-09-16 14:13:10 +1000

    fixes

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -27,7 +27,14 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 ?>
 <!-- /#admin-shortcuts -->
 
-
+<?php
+// Checks if current page is jquery-incompatible
+//echo current_path();
+if (current_path() == "node/463") { ?>
+<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
+<script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.expander.js"></script>
+<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.expander.js"></script>
+<?php } ?>
 
 <!--Drupal Jquery (1.8)-->
 <!--
@@ -77,14 +84,7 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
   </div>
 
 </div>
-<?php
-// Checks if current page is jquery-incompatible
-//echo current_path();
-if (current_path() == "node/463") { ?>
-<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.expander.js"></script>
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.expander.js"></script>
-<?php } ?>
+
 <script type="text/javascript">
 
 jQuery(document).ready(function($) {
