63a6e35fd66720354cbf354ca66e04c9804f06d8
Matt Withoos <mathewwithoos@MATHEWs-MacBook-Air.local>
2014-09-16 14:02:29 +1000

    who we are fix 2

--- themes/fial_theme/templates/page.tpl.php
+++ themes/fial_theme/templates/page.tpl.php
@@ -124,14 +124,7 @@ jQuery(document).ready(function($) {
 }
 
 </style>
-<?php
-// Checks if current page is jquery-incompatible
-//echo current_path();
-if (current_path() == "node/463") { ?>
-<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.expander.js"></script>
-<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.expander.js"></script>
-<?php } ?>
+
 <!-- new Header Top block -->
 <?php if (($page['header_top_middle']) || ($page['search_box']) || ($page['header_bottom_left']) || ($page['header_bottom_right'])): ?>
   <div id="header" class="clearfix header visible-desktop hidden-phone" role="banner">
@@ -191,7 +184,14 @@ if (current_path() == "node/463") { ?>
       <!--</div>-->
 </div>
 <?php endif; ?>
-
+<?php
+// Checks if current page is jquery-incompatible
+//echo current_path();
+if (current_path() == "node/463") { ?>
+<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
+<script type="text/javascript" src="/sites/default/themes/fial_theme/js/jquery.expander.js"></script>
+<script type="text/javascript" src="/sites/default/themes/fial_theme/js/custom-jquery.expander.js"></script>
+<?php } ?>
 <div id="main" class="clearfix main" role="main">
   <div class="container">
     <?php if ($breadcrumb): ?><?php echo $mobile['block_menu']; ?>
