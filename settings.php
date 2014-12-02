cc0de645fec1e7917f952fa6dadd756bbb665681
Reshma Neerukonda <reshma_n8@yahoo.com>
2014-12-02 16:36:55 +1100

    Scoopit API

---
+++ settings.php
@@ -0,0 +1,22 @@
+<?php
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'fialdev',
+      'username' => 'root',
+      'password' => 'roooot',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+$update_free_access = FALSE;
+$drupal_hash_salt = 'jSHxeJ8Fbm5VuufkD4bJW4CtLPHCKEgKir5drGH0gNc';
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+ini_set('session.gc_maxlifetime', 200000);
+ini_set('session.cookie_lifetime', 2000000);
